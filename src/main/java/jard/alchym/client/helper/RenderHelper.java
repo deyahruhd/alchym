7960840de4a7c191ecd2f0f977a064c9b69b2f77
Jared Barocsi <deyahruhd@gmail.com>
2021-08-22 22:10:41 -0700

    Add lightning VFX

--- src/main/java/jard/alchym/client/helper/RenderHelper.java
+++ src/main/java/jard/alchym/client/helper/RenderHelper.java
@@ -2,10 +2,9 @@ package jard.alchym.client.helper;
 
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
+import jard.alchym.AlchymReference;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.DiffuseLighting;
-import net.minecraft.client.render.OverlayTexture;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.*;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformation;
@@ -13,8 +12,12 @@ import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.texture.TextureManager;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.Matrix4f;
-import net.minecraft.util.math.Vec3f;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Pair;
+import net.minecraft.util.math.*;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 /***
  *  RenderHelper
@@ -74,5 +77,152 @@ public class RenderHelper {
 
         stack.pop ();
         RenderSystem.applyModelViewMatrix();
+    }
+
+    public static MatrixStack getViewProjectMatrix (Camera camera, double fov) {
+        MatrixStack toEyeSpace = new MatrixStack ();
+        toEyeSpace.multiply (Vec3f.POSITIVE_X.getDegreesQuaternion (camera.getPitch ()));
+        toEyeSpace.multiply (Vec3f.POSITIVE_Y.getDegreesQuaternion (camera.getYaw () + 180.f));
+        toEyeSpace.translate (-camera.getPos ().x, -camera.getPos ().y, -camera.getPos ().z);
+
+        return toEyeSpace;
+    }
+
+    private static final double[] LIGHTNING_STEPS = {
+            0.25,
+            0.5,
+            0.75,
+            1.,
+            2.5
+    };
+
+    private static final ArrayList <Pair <Vec2f, Vec2f>> LIGHTNING_QUAD_INFO = new ArrayList <> ();
+    static {
+        LIGHTNING_QUAD_INFO.add (
+                new Pair (new Vec2f (0.f / 32.f, 0.f / 32.f), new Vec2f (2.f / 32.f, 8.f / 32.f)));
+        LIGHTNING_QUAD_INFO.add (
+                new Pair (new Vec2f (2.f / 32.f, 0.f / 32.f), new Vec2f (4.f / 32.f, 8.f / 32.f)));
+        LIGHTNING_QUAD_INFO.add (
+                new Pair (new Vec2f (6.f / 32.f, 0.f / 32.f), new Vec2f (6.f / 32.f, 8.f / 32.f)));
+        LIGHTNING_QUAD_INFO.add (
+                new Pair (new Vec2f (12.f / 32.f, 0.f / 32.f), new Vec2f (8.f / 32.f, 8.f / 32.f)));
+        LIGHTNING_QUAD_INFO.add (
+                new Pair (new Vec2f (0.f / 32.f, 16.f / 32.f), new Vec2f (20.f / 32.f, 8.f / 32.f)));
+    };
+
+    private static final Random random = new Random ();
+    private static final Identifier LIGHTNING_TEXTURE = new Identifier (AlchymReference.MODID, "textures/lightning.png");
+
+    public static void renderLightning (MatrixStack stack, Vec3d start, Vec3d end, float tickDelta) {
+        boolean alternate = tickDelta > 0.85;
+
+        Vec3i color = alternate ? new Vec3i (255, 135, 190) : new Vec3i (255, 225, 255);
+
+        random.setSeed ((int) ((MinecraftClient.getInstance ().player.age) / 2.f) + (int) (tickDelta * 2.f));
+
+        VertexConsumerProvider.Immediate outlineProvider = MinecraftClient.getInstance ().getBufferBuilders ().getEffectVertexConsumers ();
+
+        VertexConsumer consumer = outlineProvider.getBuffer (RenderLayer.getEnergySwirl (LIGHTNING_TEXTURE, 0.f, 0.f));
+        Matrix4f modelMatrix = stack.peek ().getModel ();
+        Matrix3f normalMatrix = stack.peek ().getNormal ();
+        Matrix4f inverted = modelMatrix.copy ();
+        inverted.invert ();
+
+        Vector4f transformedEye = new Vector4f (0.f, 0.f, 0.f, 1.f);
+        transformedEye.transform (inverted);
+        Vec3d eye = new Vec3d (transformedEye.getX (), transformedEye.getY (), transformedEye.getZ ());
+
+        Vec3d currentPoint = start;
+        Vec3d dir = end.subtract (currentPoint);
+
+        int iteration = 0;
+
+        for (; dir.length () > LIGHTNING_STEPS [0]; ) {
+            double indexD = random.nextDouble ();
+            indexD *= indexD;
+            int index = (int) Math.floor (indexD * 5.);
+            if (iteration == 0)
+                index = 4;
+
+            if (LIGHTNING_STEPS [index] > dir.length ()) {
+                for (index = 0; index < 4; ++ index) {
+                    if (LIGHTNING_STEPS [index] > dir.length ())
+                        break;
+                }
+            }
+
+            Pair <Vec2f, Vec2f> quadInfo = LIGHTNING_QUAD_INFO.get (index);
+            Vec2f textureOrigin = quadInfo.getLeft ();
+            Vec2f textureSize   = quadInfo.getRight ();
+
+            double minStep = LIGHTNING_STEPS [index];
+            double step = Math.min (minStep, dir.length ());
+            double stepCross = random.nextDouble () * step / LIGHTNING_STEPS [4] * 1.25;
+            if (iteration == 0)
+                stepCross /= 3.;
+            double stepInDir = Math.sqrt (step * step - stepCross * stepCross);
+
+            Vec3d displacement = new Vec3d (random.nextDouble () - 0.5, random.nextDouble () - 0.5, random.nextDouble () - 0.5)
+                    .crossProduct (dir).normalize ().multiply (stepCross)
+                    .add (dir.normalize ().multiply (stepInDir));
+            Vec3d toEye = eye.subtract (currentPoint);
+            Vec3d cross = toEye.crossProduct (displacement).normalize ().multiply (0.5);
+            Vec3d prevPoint = currentPoint;
+            currentPoint = currentPoint.add (displacement);
+
+            Vec3d corner1 = currentPoint.add (cross);
+            Vec3d corner2 = prevPoint.add (cross);
+            Vec3d corner3 = prevPoint.subtract (cross);
+            Vec3d corner4 = currentPoint.subtract (cross);
+
+            float xShift = ((float) MinecraftClient.getInstance ().player.age + tickDelta) / 15.f;
+
+            if (alternate) {
+                xShift = - xShift;
+            }
+
+            float u1 = textureOrigin.x + (xShift % 2.f);
+            float u2 = textureOrigin.x + (textureSize.x * (float) (step / minStep)) + (xShift % 2.f);
+            float v1 = textureOrigin.y;
+            float v2 = textureOrigin.y + textureSize.y;
+
+            double flipChance = random.nextDouble ();
+            if (alternate) {
+                v1 = v2;
+                v2 = textureOrigin.y;
+            }
+
+            consumer.vertex (modelMatrix, (float) corner1.x, (float) corner1.y, (float) corner1.z)
+                    .color (color.getX (), color.getY (), color.getZ (), 255)
+                    .texture (u1, v1)
+                    .overlay (OverlayTexture.DEFAULT_UV)
+                    .light (15)
+                    .normal (0.f, 0.f, 0.f)
+                    .next ();
+            consumer.vertex (modelMatrix, (float) corner2.x, (float) corner2.y, (float) corner2.z)
+                    .color (color.getX (), color.getY (), color.getZ (), 255)
+                    .texture (u2, v1)
+                    .overlay (OverlayTexture.DEFAULT_UV)
+                    .light (15)
+                    .normal (0.f, 0.f, 0.f)
+                    .next ();
+            consumer.vertex (modelMatrix, (float) corner3.x, (float) corner3.y, (float) corner3.z)
+                    .color (color.getX (), color.getY (), color.getZ (), 255)
+                    .texture (u2, v2)
+                    .overlay (OverlayTexture.DEFAULT_UV)
+                    .light (15)
+                    .normal (0.f, 0.f, 0.f)
+                    .next ();
+            consumer.vertex (modelMatrix, (float) corner4.x, (float) corner4.y, (float) corner4.z)
+                    .color (color.getX (), color.getY (), color.getZ (), 255)
+                    .texture (u1, v2)
+                    .overlay (OverlayTexture.DEFAULT_UV)
+                    .light (15)
+                    .normal (0.f, 0.f, 0.f)
+                    .next ();
+
+            dir = end.subtract (currentPoint);
+            iteration ++;
+        }
     }
 }
--- src/main/java/jard/alchym/mixin/rendering/ItemUseAnimMixin.java
+++ src/main/java/jard/alchym/mixin/rendering/ItemUseAnimMixin.java
@@ -3,11 +3,11 @@ package jard.alchym.mixin.rendering;
 import jard.alchym.client.MatrixStackAccess;
 import jard.alchym.client.MinecraftClientDataAccess;
 import jard.alchym.items.CustomAttackItem;
-import jard.alchym.items.RevolverItem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
@@ -15,6 +15,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Arm;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.Vec3f;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -31,6 +32,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin (HeldItemRenderer.class)
 public abstract class ItemUseAnimMixin {
     private static float swayProgress;
+    @Shadow
+    @Final
+    private ItemRenderer itemRenderer;
 
     @Shadow
     public void renderItem (LivingEntity livingEntity, ItemStack itemStack, ModelTransformation.Mode mode, boolean bl, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {}
@@ -49,7 +53,6 @@ public abstract class ItemUseAnimMixin {
         Arm arm = (hand == Hand.MAIN_HAND) ? player.getMainArm () : player.getMainArm ().getOpposite ();
 
         if (heldItem.getItem () instanceof CustomAttackItem) {
-
             applyEquipOffset (matrixStack, arm, equipProgress);
 
             if (((CustomAttackItem) heldItem.getItem ()).autoUse (heldItem)) {
@@ -77,6 +80,40 @@ public abstract class ItemUseAnimMixin {
             }
 
             ((MatrixStackAccess) matrixStack).multiply (((CustomAttackItem) heldItem.getItem ()).getAnimMatrix (heldItem, arm, swingProgress));
+
+            /* TODO: This needs to be put somewhere better
+            float projectileSpeed = MovementHelper.upsToSpt (975.f * 15.3f);
+
+            Camera camera = MinecraftClient.getInstance ().gameRenderer.getCamera ();
+            Vec3d eyePos = camera.getPos ();
+            Vec3d initialSpawnPos = player.getRotationVecClient ().multiply (projectileSpeed * 2.f).add (eyePos);
+
+            // Trace from player eye pos to projectile spawn position
+            BlockHitResult cast = player.world.raycast (new RaycastContext (eyePos, initialSpawnPos, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.ANY, player));
+
+            if (cast.getType () != HitResult.Type.MISS) {
+                Vec3d start = new Vec3d (0.0, 0.165, -0.08);
+                Vec3d end = cast.getPos ();
+
+                Vector4f transformedStart = new Vector4f ((float) start.x, (float) start.y, (float) start.z, 1.f);
+                MatrixStack modelTransform = new MatrixStack ();
+                itemRenderer.getHeldItemModel (heldItem, player.world, player, 0).getTransformation ()
+                        .getTransformation (arm == Arm.RIGHT ? ModelTransformation.Mode.FIRST_PERSON_RIGHT_HAND : ModelTransformation.Mode.FIRST_PERSON_LEFT_HAND)
+                        .apply (arm == Arm.LEFT, modelTransform);
+                transformedStart.transform (modelTransform.peek ().getModel ());
+                start = new Vec3d (transformedStart.getX (), transformedStart.getY (), transformedStart.getZ ());
+
+                Vector4f transformedEnd = new Vector4f ((float) end.x, (float) end.y, (float) end.z, 1.f);
+                MatrixStack viewMatrix = RenderHelper.getViewProjectMatrix (camera, MinecraftClient.getInstance ().options.fov);
+                Matrix4f invertedBobbing = matrixStack.peek ().getModel ().copy ();
+                invertedBobbing.invert ();
+                transformedEnd.transform (viewMatrix.peek ().getModel ());
+                transformedEnd.transform (invertedBobbing);
+                end = new Vec3d (transformedEnd.getX (), transformedEnd.getY (), transformedEnd.getZ ());
+
+                RenderHelper.renderLightning (matrixStack, start, end, tickDelta);
+            }
+            */
 
             this.renderItem (player, heldItem, arm == Arm.RIGHT ? ModelTransformation.Mode.FIRST_PERSON_RIGHT_HAND : ModelTransformation.Mode.FIRST_PERSON_LEFT_HAND, arm == Arm.LEFT, matrixStack, vertexConsumerProvider, j);
 
